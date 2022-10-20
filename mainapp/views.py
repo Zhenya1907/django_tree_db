b6d2b9ab71b2745bf2984ece01c371df16dbd80c
zhenya <e.group550@gmail.com>
2022-10-21 00:50:45 +0400

    added urls file  and simple view

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
 
-# Create your views here.
+
+class Index(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse("Hello world")
