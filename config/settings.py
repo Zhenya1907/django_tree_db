900ebf27c159f574fe157e3b46c500afb6acf147
zhenya <e.group550@gmail.com>
2022-10-21 16:43:34 +0400

    added psycopg2 and fixed misprint in models

--- config/settings.py
+++ config/settings.py
@@ -75,13 +75,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydatabase",
+        "USER": "mydatabaseuser",
+        "PASSWORD": "mypassword",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
--- mainapp/models.py
+++ mainapp/models.py
@@ -2,22 +2,21 @@ from django.db import models
 
 
 class Department(models.Model):
-    name = models.CharField(max_lenght=50, verbose_name='Department')
+    name = models.CharField(max_length=50, verbose_name="Department")
     parent_id = models.IntegerField()
 
     def __str__(self) -> str:
-        return f'{self.pk} {self.name} {self.parent_id}'
+        return f"{self.pk} {self.name} {self.parent_id}"
 
 
 class Employee(models.Model):
-    first_name = models.CharField(max_lenght=10, verbose_name='First name')
-    middle_name = models.CharField(max_lenght=15, verbose_name='Middle name')
-    last_name = models.CharField(max_lenght=20, verbose_name='Last name')
-    position = models.CharField(max_lenght=50, verbose_name='Position')
-    employment_date = models.DateTimeField(
-        auto_now_add=True, verbose_name='Employment date')
-    salary = models.DecimalField(max_digits=6, decimal_places=0, verbose_name='Salary', default=0)
+    first_name = models.CharField(max_length=10, verbose_name="First name")
+    middle_name = models.CharField(max_length=5, verbose_name="Middle name")
+    last_name = models.CharField(max_length=20, verbose_name="Last name")
+    position = models.CharField(max_length=50, verbose_name="Position")
+    employment_date = models.DateTimeField(auto_now_add=True, verbose_name="Employment date")
+    salary = models.DecimalField(max_digits=6, decimal_places=0, verbose_name="Salary", default=0)
     department_id = models.ForeignKey(Department)
 
     def __str__(self) -> str:
-        return f'{self.pk} {self.first_name}'
+        return f"{self.pk} {self.first_name}"
